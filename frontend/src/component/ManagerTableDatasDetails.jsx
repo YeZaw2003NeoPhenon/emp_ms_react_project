f22290257535c113d89d13bdf4443b9d371dc755
Neo <148946248+NeoPhenon@users.noreply.github.com>
2024-10-22 14:24:10 +0630

    toned-down

--- frontend/src/component/ManagerTableDatasDetails.jsx
+++ frontend/src/component/ManagerTableDatasDetails.jsx
@@ -1,20 +1,28 @@
-import { Modal,Button , ModalHeader , ModalFooter , ModalTitle , ModalBody } from "react-bootstrap"
+import { Modal, Button , ModalHeader , ModalFooter , ModalTitle , ModalBody } from "react-bootstrap"
+import {Link, useHistory } from "react-router-dom"
 export const ManagerTableDatasDetails = ({manager , showModal , handleShowModal , handleCloseModal , handleDelete }) => {
+  const history = useHistory()
+
+  const managerUpdatePath = () => {
+    history.push(`/manager/edit/${manager.id}`)
+  }
+
     return(
        <> 
        <tr>
               <td>{manager.id}</td>
               <td>{manager.name}</td>
               <td>{manager.department}</td>
-              <td> 
-                <Button variant = "danger" className = "btn btn-md" onClick={() => handleShowModal(manager.id)} >Delete Manager</Button>
+              <td  className = " btn-group d-flex align-items-center justify-content-center gap-3 "> 
+                <Button variant = "danger" className = "btn btn-md w-50 rounded-pill" onClick={() => handleShowModal(manager.id)} >Delete Manager</Button>
+                <Button variant = "secondary" as= {Link} to={`/manager/edit/${manager.id}`} className = "btn btn-md mr-3 w-50 rounded-pill" onClick={managerUpdatePath} target = "_parent">Update</Button>
               </td>
         </tr>
-              <Modal
+            <Modal
               show={showModal}
               onHide={handleCloseModal}
               backdrop="static"
-            >   
+            >  
             <ModalHeader closeButton>
                 <ModalTitle>Delete Manager Data</ModalTitle>
               </ModalHeader>
